ca7769778bdcfe18dc8e2a91e5b04d1064a35468
firstareeb <142777204+firstareeb@users.noreply.github.com>
2024-01-02 16:52:33 +0530

    Update program13.py

--- program13.py
+++ program13.py
@@ -1,1 +1,6 @@
-
+# 1.3 Write a program to find whether a given no is even and odd.
+num = int(input("Enter a number: "))
+if num % 2 == 0:
+    print(f"The number {num} is Even.")
+else:
+    print(f"The number {num} is Odd.")
